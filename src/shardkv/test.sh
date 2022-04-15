565f8ac382cf184053e464ea9115f1f03dc15e1a
YowKuan <yoyo6213@gmail.com>
2022-04-15 18:19:56 -0400

    [hw4b v2]

--- src/shardkv/test.sh
+++ src/shardkv/test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-for t in TestConcurrentUnreliable # add all the test names here
+for t in TestBasic TestMove TestLimp TestConcurrent TestConcurrentUnreliable # add all the test names here
 do
   echo $t
   count=0
-  n=20
+  n=50
   for i in $(seq 1 $n)
   do
     go test -run "^${t}$" -timeout 2m > ./log-${t}-${i}.txt
